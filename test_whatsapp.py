2788ba5b9a65790bd1a0a74129528905aaecffd3
Akash <akash32bhardwaj@gmail.com>
2026-09-21 12:57:19 +0530

    Tidy: remove backups, duplicates, test scripts and debug images

--- test_whatsapp.py
+++
@@ -1,6 +0,0 @@
-python -c "
-from alert_settings import settings
-print('notify_blacklisted:', settings.get('notify_blacklisted'))
-print('should alert BLACKLISTED:', settings.should_alert_security('BLACKLISTED'))
-print('security number:', settings.get('security_whatsapp'))
-"
